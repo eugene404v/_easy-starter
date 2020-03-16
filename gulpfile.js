f5325c236bd5d2825dd24b96c309275ec06278fe
Eugene Koryabkin <eugenewalknut@gmail.com>
2020-03-16 19:05:37 +0300

    fix dist dir

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const fileinclude = require('gulp-file-include');
-
-
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const pug = require('gulp-pug');
 const webpack = require("webpack-stream");
 
 
@@ -20,28 +21,36 @@ const dist = "./dist/";
 
 
 
-gulp.task('html', function(callback) {
+gulp.task('html', function (callback) {
     return gulp.src('./src/html/*.html')
         .pipe(plumber({
-            errorHandler: notify.onError(function(err){
-                return{
+            errorHandler: notify.onError(function (err) {
+                return {
                     title: 'HTML include',
                     sound: false,
                     message: err.message
                 };
             })
         }))
-        .pipe(fileinclude({prefix: '@@'}))
-      .pipe(gulp.dest('./build/'))
+        .pipe(fileinclude({
+            prefix: '@@'
+        }))
+        .pipe(gulp.dest('./dist/'))
     callback();
+});
+
+gulp.task('pug', function () {
+    gulp.src('src/pug/*.pug')
+        .pipe(pug({pretty: '\t'}))
+        .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('sass', function (callback) {
     return gulp.src('./src/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(plumber({
-            errorHandler: notify.onError(function(err){
-                return{
+            errorHandler: notify.onError(function (err) {
+                return {
                     title: 'SCSS compilation',
                     sound: false,
                     message: err.message
@@ -53,76 +62,93 @@ gulp.task('sass', function (callback) {
             overrideBrowserslist: ['last 4 versions']
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./build/css/'))
+        .pipe(csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        }))
+        .pipe(gulp.dest('./dist/css/'))
     callback();
 });
 
 //gulp.task('copy:img', function(callback) {
- //   return gulp.src('./src/img/**/*.*')
-  //    .pipe(gulp.dest('./build/img/'))
-   //   callback();
+//   return gulp.src('./src/img/**/*.*')
+//    .pipe(gulp.dest('./build/img/'))
+//   callback();
 //});
 
 //gulp.task('copy:js', function(callback) {
-   // return gulp.src('./src/js/**/*.*')
-   //   .pipe(gulp.dest('./build/js/'))
-   //   callback();
+// return gulp.src('./src/js/**/*.*')
+//   .pipe(gulp.dest('./build/js/'))
+//   callback();
 //});
 
 gulp.task("build-js", () => {
     return gulp.src("./src/js/main.js")
-                .pipe(webpack({
-                    mode: 'development',
-                    output: {
-                        filename: 'script.js'
-                    },
-                    watch: false,
-                    devtool: "source-map",
-                    module: {
-                        rules: [
-                          {
-                            test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'script.js'
+            },
+            watch: false,
+            devtool: "source-map",
+            module: {
+                rules: [{
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {
                                     debug: true,
                                     corejs: 3,
                                     useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
-                          }
-                        ]
-                      }
-                }))
-                .pipe(gulp.dest(dist))
-                .on("end", browserSync.reload);
+                                }]
+                            ]
+                        }
+                    }
+                }]
+            }
+        }))
+        .pipe(gulp.dest(dist))
+        .on("end", browserSync.reload);
 });
 
 gulp.task("copy-assets", () => {
     return gulp.src("./src/assets/**/*.*")
-                .pipe(gulp.dest(dist + "/assets"))
-                .on("end", browserSync.reload);
+        .pipe(gulp.dest(dist + "/assets"))
+        .on("end", browserSync.reload);
 });
 
+gulp.task('webp', () =>
+    gulp.src('src/assets/**/*.*')
+    .pipe(webp())
+    .pipe(gulp.dest('dist'))
+);
 
+gulp.task('compress', function(done) {
+    gulp.src('src/assets/**/*.**')
+    .pipe(imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist'))
+  done();
+  });
 
 
 gulp.task('watch', function () {
-    watch(['./build/*.html', './build/css/**/*.css'], gulp.parallel(browserSync.reload));
-    watch('./src/scss/**/*.scss', function(){
+    watch(['./dist/*.html', './dist/css/**/*.css'], gulp.parallel(browserSync.reload));
+    watch('./src/scss/**/*.scss', function () {
         setTimeout(gulp.parallel('sass'), 1000);
     });
     watch('./src/html/**/*.html', gulp.parallel('html'));
+    watch("./src/pug/**/*.pug", gulp.parallel("pug"));
     watch("./src/js/**/*.js", gulp.parallel("build-js"));
 });
 
 gulp.task('server', function () {
     browserSync.init({
         server: {
-            baseDir: "./build/"
+            baseDir: "./dist/"
         }
     });
 });
